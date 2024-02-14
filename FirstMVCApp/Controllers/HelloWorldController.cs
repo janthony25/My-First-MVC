3b8e72d9683a1bb3ffa04a959990f614c40636fe
janthony25 <junanthony.business@gmail.com>
2024-02-14 23:37:09 +1300

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public string Index()
+        {
+            return "This is the Index page!";
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
